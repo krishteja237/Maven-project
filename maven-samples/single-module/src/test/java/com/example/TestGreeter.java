4de7ff071820ffeef0538dd0240a58172bdeb9ca
Krishna Teja <Krishteja237@gmail.com>
2021-11-21 14:54:57 -0500

    This is a new change to the addition

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingfun() {
     String someone = "World, Thank you";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
